c6eda8187d4ffed2b9d1a7b413562b8abbb41a56
Drew <drew.seminara@gartrellgroup.com>
2018-02-06 10:29:46 -0800

    add the ability to change colors of drawn features

--- assets/js/map.js
+++ assets/js/map.js
@@ -260,7 +260,6 @@ map.on('load', function () {
     });
 
 
-
     //Layer Info function
     //Layer Info function
     //Layer Info function
@@ -1008,21 +1007,416 @@ populatePalette();
 map.on('click', addEditLabels);
 
 
+// custom draw styles paramaters
+// custom draw styles paramaters
+// custom draw styles paramaters
+var drawFeatureID = '';
+var newDrawFeature = false;
+
 //Draw Tools function
 //Draw Tools function
 //Draw Tools function
 var draw = new MapboxDraw({
+    // this is used to allow for custom properties for styling draw features
+    // it appends the word "user_" to the property
+    userProperties: true,
     displayControlsDefault: false,
     controls: {
         polygon: true,
         point: true,
         line_string: true,
         trash: true,
-    }
+    },
+    styles: [
+        // default themes provided by MB Draw
+        // default themes provided by MB Draw
+        // default themes provided by MB Draw
+        // default themes provided by MB Draw
+
+
+        {
+            'id': 'gl-draw-polygon-fill-inactive',
+            'type': 'fill',
+            'filter': ['all', ['==', 'active', 'false'],
+                ['==', '$type', 'Polygon'],
+                ['!=', 'mode', 'static']
+            ],
+            'paint': {
+                'fill-color': '#3bb2d0',
+                'fill-outline-color': '#3bb2d0',
+                'fill-opacity': 0.1
+            }
+        },
+        {
+            'id': 'gl-draw-polygon-fill-active',
+            'type': 'fill',
+            'filter': ['all', ['==', 'active', 'true'],
+                ['==', '$type', 'Polygon']
+            ],
+            'paint': {
+                'fill-color': '#fbb03b',
+                'fill-outline-color': '#fbb03b',
+                'fill-opacity': 0.1
+            }
+        },
+        {
+            'id': 'gl-draw-polygon-midpoint',
+            'type': 'circle',
+            'filter': ['all', ['==', '$type', 'Point'],
+                ['==', 'meta', 'midpoint']
+            ],
+            'paint': {
+                'circle-radius': 3,
+                'circle-color': '#fbb03b'
+            }
+        },
+        {
+            'id': 'gl-draw-polygon-stroke-inactive',
+            'type': 'line',
+            'filter': ['all', ['==', 'active', 'false'],
+                ['==', '$type', 'Polygon'],
+                ['!=', 'mode', 'static']
+            ],
+            'layout': {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            'paint': {
+                'line-color': '#3bb2d0',
+                'line-width': 2
+            }
+        },
+        {
+            'id': 'gl-draw-polygon-stroke-active',
+            'type': 'line',
+            'filter': ['all', ['==', 'active', 'true'],
+                ['==', '$type', 'Polygon']
+            ],
+            'layout': {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            'paint': {
+                'line-color': '#fbb03b',
+                'line-dasharray': [0.2, 2],
+                'line-width': 2
+            }
+        },
+        {
+            'id': 'gl-draw-line-inactive',
+            'type': 'line',
+            'filter': ['all', ['==', 'active', 'false'],
+                ['==', '$type', 'LineString'],
+                ['!=', 'mode', 'static']
+            ],
+            'layout': {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            'paint': {
+                'line-color': '#3bb2d0',
+                'line-width': 2
+            }
+        },
+        {
+            'id': 'gl-draw-line-active',
+            'type': 'line',
+            'filter': ['all', ['==', '$type', 'LineString'],
+                ['==', 'active', 'true']
+            ],
+            'layout': {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            'paint': {
+                'line-color': '#fbb03b',
+                'line-dasharray': [0.2, 2],
+                'line-width': 2
+            }
+        },
+        {
+            'id': 'gl-draw-polygon-and-line-vertex-stroke-inactive',
+            'type': 'circle',
+            'filter': ['all', ['==', 'meta', 'vertex'],
+                ['==', '$type', 'Point'],
+                ['!=', 'mode', 'static']
+            ],
+            'paint': {
+                'circle-radius': 5,
+                'circle-color': '#fff'
+            }
+        },
+        {
+            'id': 'gl-draw-polygon-and-line-vertex-inactive',
+            'type': 'circle',
+            'filter': ['all', ['==', 'meta', 'vertex'],
+                ['==', '$type', 'Point'],
+                ['!=', 'mode', 'static']
+            ],
+            'paint': {
+                'circle-radius': 3,
+                'circle-color': '#fbb03b'
+            }
+        },
+        {
+            'id': 'gl-draw-point-point-stroke-inactive',
+            'type': 'circle',
+            'filter': ['all', ['==', 'active', 'false'],
+                ['==', '$type', 'Point'],
+                ['==', 'meta', 'feature'],
+                ['!=', 'mode', 'static']
+            ],
+            'paint': {
+                'circle-radius': 5,
+                'circle-opacity': 1,
+                'circle-color': '#fff'
+            }
+        },
+        {
+            'id': 'gl-draw-point-inactive',
+            'type': 'circle',
+            'filter': ['all', ['==', 'active', 'false'],
+                ['==', '$type', 'Point'],
+                ['==', 'meta', 'feature'],
+                ['!=', 'mode', 'static']
+            ],
+            'paint': {
+                'circle-radius': 3,
+                'circle-color': '#3bb2d0'
+            }
+        },
+        {
+            'id': 'gl-draw-point-stroke-active',
+            'type': 'circle',
+            'filter': ['all', ['==', '$type', 'Point'],
+                ['==', 'active', 'true'],
+                ['!=', 'meta', 'midpoint']
+            ],
+            'paint': {
+                'circle-radius': 7,
+                'circle-color': '#fff'
+            }
+        },
+        {
+            'id': 'gl-draw-point-active',
+            'type': 'circle',
+            'filter': ['all', ['==', '$type', 'Point'],
+                ['!=', 'meta', 'midpoint'],
+                ['==', 'active', 'true']
+            ],
+            'paint': {
+                'circle-radius': 5,
+                'circle-color': '#fbb03b'
+            }
+        },
+        {
+            'id': 'gl-draw-polygon-fill-static',
+            'type': 'fill',
+            'filter': ['all', ['==', 'mode', 'static'],
+                ['==', '$type', 'Polygon']
+            ],
+            'paint': {
+                'fill-color': '#404040',
+                'fill-outline-color': '#404040',
+                'fill-opacity': 0.1
+            }
+        },
+        {
+            'id': 'gl-draw-polygon-stroke-static',
+            'type': 'line',
+            'filter': ['all', ['==', 'mode', 'static'],
+                ['==', '$type', 'Polygon']
+            ],
+            'layout': {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            'paint': {
+                'line-color': '#404040',
+                'line-width': 2
+            }
+        },
+        {
+            'id': 'gl-draw-line-static',
+            'type': 'line',
+            'filter': ['all', ['==', 'mode', 'static'],
+                ['==', '$type', 'LineString']
+            ],
+            'layout': {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            'paint': {
+                'line-color': '#404040',
+                'line-width': 2
+            }
+        },
+        {
+            'id': 'gl-draw-point-static',
+            'type': 'circle',
+            'filter': ['all',
+                ['==', 'mode', 'static'],
+                ['==', '$type', 'Point']
+            ],
+            'paint': {
+                'circle-radius': 5,
+                'circle-color': '#404040'
+            }
+        },
+
+        // end default themes provided by MB Draw
+        // end default themes provided by MB Draw
+        // end default themes provided by MB Draw
+        // end default themes provided by MB Draw
+
+
+
+
+        // new styles for toggling colors
+        // new styles for toggling colors
+        // new styles for toggling colors
+        // new styles for toggling colors
+
+        {
+            'id': 'gl-draw-polygon-color-picker',
+            'type': 'fill',
+            'filter': ['all', ['==', '$type', 'Polygon'],
+                ['has', 'user_portColor']
+            ],
+            'paint': {
+                'fill-color': ['get', 'user_portColor'],
+                'fill-outline-color': ['get', 'user_portColor'],
+                'fill-opacity': 0.5
+            }
+        },
+        {
+            'id': 'gl-draw-polygon-stroke-color-picker',
+            'type': 'line',
+            'filter': ['all', ['==', '$type', 'Polygon'],
+                ['has', 'user_portColor']
+            ],
+            'layout': {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            'paint': {
+                'line-color': ['get', 'user_portColor'],
+                'line-width': 2
+            }
+        },
+        {
+            'id': 'gl-draw-line-color-picker',
+            'type': 'line',
+            'filter': ['all', ['==', '$type', 'LineString'],
+                ['has', 'user_portColor']
+            ],
+            'paint': {
+                'line-color': ['get', 'user_portColor'],
+                'line-width': 2
+            }
+        },
+        {
+            'id': 'gl-draw-polygon-and-line-vertex-color-picker',
+            'type': 'circle',
+            'filter': ['all', ['==', 'meta', 'vertex'],
+                ['==', '$type', 'Point'],
+                ['!=', 'mode', 'static'],
+                ['has', 'user_portColor']
+            ],
+            'paint': {
+                'circle-radius': 3,
+                'circle-color': ['get', 'user_portColor']
+            }
+        },
+        {
+            'id': 'gl-draw-point-color-picker',
+            'type': 'circle',
+            'filter': ['all',
+                ['==', '$type', 'Point'],
+                ['has', 'user_portColor']
+            ],
+            'paint': {
+                'circle-radius': 3,
+                'circle-color': ['get', 'user_portColor']
+            }
+        },
+        {
+            'id': 'gl-draw-point-active-color-picker',
+            'type': 'circle',
+            'filter': ['all', ['==', '$type', 'Point'],
+                ['!=', 'meta', 'midpoint'],
+                ['==', 'active', 'true'],
+                ['has', 'user_portColor']
+            ],
+            'paint': {
+                'circle-radius': 5,
+                'circle-color': ['get', 'user_portColor']
+            }
+        },
+    ]
 });
 
 var drawTool = document.getElementById('drawAppend');
 drawTool.appendChild(draw.onAdd(map))
+
+// create draw palette
+function populateDrawPalette() {
+    var drawFeatureColor = document.getElementById('customDrawColor');
+
+    for (var c = 0; c < TEXT_COLORS.length; c++) {
+        var cElm = document.createElement('div');
+        cElm.className = 'draw-color-change';
+        cElm.id = 'draw-' + TEXT_COLORS[c];
+        cElm.style['background-color'] = TEXT_COLORS[c];
+        cElm.addEventListener('mousedown', changeDrawColor);
+
+        drawFeatureColor.appendChild(cElm);
+    };
+}
+
+// color change function of draw features
+var changeDrawColor = function(e) {
+    if (e.target.id && e.target.id.indexOf('draw-') === -1) return;
+
+    var color = e.target.id.replace(/draw-/, '');
+
+    if (drawFeatureID !== '' && typeof draw === 'object') {
+
+        draw.setFeatureProperty(drawFeatureID, 'portColor', color);
+
+        var feat = draw.get(drawFeatureID);
+        draw.add(feat)
+    }
+}
+
+// callback for draw.update and draw.selectionchange
+var setDrawFeature = function(e) {
+    if (e.features.length && e.features[0].type === 'Feature') {
+        var feat = e.features[0];
+        drawFeatureID = feat.id;
+    }
+}
+
+// Event Handlers for Draw Tools
+map.on('draw.create', function() {
+    newDrawFeature = true;
+});
+
+map.on('draw.update', setDrawFeature);
+
+map.on('draw.selectionchange', setDrawFeature);
+
+map.on('click', function(e) {
+    if (!newDrawFeature) {
+        var drawFeatureAtPoint = draw.getFeatureIdsAt(e.point);
+
+        //if another drawFeature is not found - reset drawFeatureID
+        drawFeatureID = drawFeatureAtPoint.length ? drawFeatureAtPoint[0] : '';
+    }
+
+    newDrawFeature = false;
+
+});
 
 
 //// Turf Area Calc
@@ -1030,20 +1424,6 @@ var selectedUnits = '';
 var selectedMeasuredFeature = '';
 var measurementActive = false;
 
-$(function(){
-    // set unit value
-    selectedUnits = $('input[type=radio][name=unit]:checked').val();
-
-    $('input[type=radio][name=unit]').change(function() {
-        selectedUnits = this.value;
-
-        //update values based on new units
-        if (selectedMeasuredFeature !== '' || measurementActive) {
-            var gj = draw.get(selectedMeasuredFeature);
-            calculateDimensions(gj);
-        }
-    })
-});
 
 function removeMeasurementValues() {
     $('#calculated-area p').remove();
@@ -1205,4 +1585,22 @@ map.on('click', function(e){
     }
 
     measurementActive = false;
+});
+
+
+$(function(){
+    // set unit value
+    selectedUnits = $('input[type=radio][name=unit]:checked').val();
+
+    $('input[type=radio][name=unit]').change(function() {
+        selectedUnits = this.value;
+
+        //update values based on new units
+        if (selectedMeasuredFeature !== '' || measurementActive) {
+            var gj = draw.get(selectedMeasuredFeature);
+            calculateDimensions(gj);
+        }
+    })
+
+    populateDrawPalette();
 });
